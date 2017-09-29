584cf846fff11a8cca56caa58f5e2bbdc4fdec34
Andrea Soper <soper@palantir.net>
2017-09-29 15:51:50 -0500

    Add some helpful output.

--- src/MidCamp/Hatter.php
+++ src/MidCamp/Hatter.php
@@ -11,26 +11,31 @@ use Composer\Script\Event;
 class Hatter
 {
   public static function installAssets(Event $event) {
-    echo "\n\n\nInstalling Hatter style-guide assets ...\n\n\n";
+    echo "\n\n\nInstalling Hatter style-guide assets ...\n\n";
 
     /** @var Composer $composer */
     $composer = $event->getComposer();
 
     // Figure out the source path.
     $vendor_path = $composer->getConfig()->get('vendor-dir');
-    $source_path = "{$vendor_path}/content/assets/";
+    $source_path = "{$vendor_path}/midcamp/hatter/src/content/assets/";
 
     // Figure out the destination path.
     $extra = $composer->getPackage()->getExtra();
     $destination_path = realpath("$vendor_path/../{$extra['styleguide']['theme-path']}");
 
     // Find the names of directories in the source directory.
-    foreach (glob($source_path . '/*' , GLOB_ONLYDIR) as $dir_name) {
-      $source = "{$source_path}/{$dir_name}/";
+    foreach (glob($source_path . '/*' , GLOB_ONLYDIR) as $source) {
+      $parts = explode("/", $source);
+      $dir_name = end($parts);
 
       // Copy all the directories in the source directory into the theme.
       if (is_dir($source)) {
         copy($source, "{$destination_path}/{$dir_name}");
+        echo "Copied $source to {$destination_path}/{$dir_name}";
+      }
+      else {
+        echo "Could not copy $source to {$destination_path}/{$dir_name}";
       }
     }
   }

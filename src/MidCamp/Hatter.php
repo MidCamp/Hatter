14ea28f8310c2d2c6de3e7298ab65485652de073
Andrea Soper <soper@palantir.net>
2017-09-29 15:42:49 -0500

    And make it work.

--- src/MidCamp/Hatter.php
+++ src/MidCamp/Hatter.php
@@ -11,11 +11,28 @@ use Composer\Script\Event;
 class Hatter
 {
   public static function installAssets(Event $event) {
+    echo "\n\n\nInstalling Hatter style-guide assets ...\n\n\n";
 
-    $extra = $event->getComposer()->getPackage()->getExtra();
-    var_dump($extra);
+    /** @var Composer $composer */
+    $composer = $event->getComposer();
 
-    echo "\n\n\nInstalling Assets for {$event->getName()}...\n\n\n";
+    // Figure out the source path.
+    $vendor_path = $composer->getConfig()->get('vendor-dir');
+    $source_path = "{$vendor_path}/content/assets/";
+
+    // Figure out the destination path.
+    $extra = $composer->getPackage()->getExtra();
+    $destination_path = realpath("$vendor_path/../{$extra['styleguide']['theme-path']}");
+
+    // Find the names of directories in the source directory.
+    foreach (glob($source_path . '/*' , GLOB_ONLYDIR) as $dir_name) {
+      $source = "{$source_path}/{$dir_name}/";
+
+      // Copy all the directories in the source directory into the theme.
+      if (is_dir($source)) {
+        copy($source, "{$destination_path}/{$dir_name}");
+      }
+    }
   }
 
 }
